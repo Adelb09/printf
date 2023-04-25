2f24bb5f928517a7a4fdea2433bb62d9f56556c2
Adelb09 <adelberhili99@gmail.com>
2023-04-25 05:57:31 -0700

    Updated file1 and added file2

--- printf.c
+++ printf.c
@@ -1,15 +1,23 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
 
 int _printf(const char *format, ...)
 {
-va_list args;
-int printed_chars;
+        va_list args;
+        int printed_chars = 0;
+        char buffer[1024];
 
-va_start(args, format);
-printed_chars = vprintf(format, args);
-va_end(args);
+        va_start(args, format);
+        printed_chars = vsprintf(buffer, format, args);
+        va_end(args);
 
-return printed_chars;
+        if (printed_chars < 0)
+                return (-1);
+
+        if (write(1, buffer, printed_chars) != printed_chars)
+                return (-1);
+
+        return (printed_chars);
 }
